9bb2515ba9dcdb38e19e5ca8b50bb2a69d945634
Sokus <soqs687@gmail.com>
2022-11-02 23:44:39 +0100

    Move from SDL/imgui to Raylib

--- src/client/client_main.cpp
+++ src/client/client_main.cpp
@@ -2,15 +2,12 @@
 #include <stdlib.h>
 
 #include "glm/glm.hpp"
-#include "imgui.h"
 
 #include "config.h"
 #include "macros.h" // ABS
 #include "filesystem.h"
-#include "input.h"
-#include "platform.h"
-#include "graphics/opengl3_renderer.h"
-#include "graphics/opengl3_draw_queue.h"
+
+#include "raylib.h"
 
 enum class Facing {LEFT, RIGHT};
 
@@ -25,7 +22,7 @@ struct Player
 
     void Update()
     {
-        position += velocity * OS::DeltaTime();
+        position += velocity * GetFrameTime();
 
         if(velocity.x < 0) facing = Facing::LEFT;
         if(velocity.x > 0) facing = Facing::RIGHT;
@@ -37,71 +34,65 @@ struct Player
     {
         glm::vec2 move_dir = glm::vec2(0.0f, 0.0f);
         // Left/right movement
-        if(Input::KeyDown(Input::Keybind::Left)) move_dir.x -=1.0f;
-        if(Input::KeyDown(Input::Keybind::Right)) move_dir.x += 1.0f;
+        if(IsKeyDown(KEY_A)) move_dir.x -= 1.0f;
+        if(IsKeyDown(KEY_D)) move_dir.x += 1.0f;
 
         velocity.x = move_speed * move_dir.x;
     }
 };
 
-const float Player::move_speed = 4.0f;
+const float Player::move_speed = 100.0f;
 
 int main(int, char**)
 {
-    OS::CreateContext("Nie patrz mi sie na tytul", 960, 540);
+    InitWindow(960, 540, "PI");
+    SetTargetFPS(60);
 
-    int screen_width, screen_height;
     Player player;
     glm::vec2 camera_p = glm::vec2(0.0f, 0.0f);
 
-    Texture texture;
-    texture.Load(RESOURCE_PATH "/character.png", 16, 24, 4);
+    Texture2D character = LoadTexture(RESOURCE_PATH "/character.png");
 
-    Renderer renderer;
-    DrawQueue draw_queue;
-
-    OS::GetWindowDimensions(&screen_width, &screen_height);
-
-    while(OS::IsRunning())
+    while(!WindowShouldClose())
     {
-        OS::BeginFrame();
-        OS::GetWindowDimensions(&screen_width, &screen_height);
-
         player.Control();
         player.Update();
 
-        draw_queue.Clear();
-        draw_queue.SetViewportDimensions({screen_width, screen_height});
-        draw_queue.SetViewMatrix(glm::mat4(1.0f));
-        draw_queue.SetProjectionMatrix(CreateProjectionMatrix(screen_width, screen_height, 4.0f));
-        draw_queue.SetBackgroundColor({0.1f, 0.15f, 0.2f});
-        draw_queue.SetAmbientLightColor({0.1f, 0.15f, 0.2f});
+        BeginDrawing();
+            ClearBackground(Color{25, 30, 40});
 
-        // Render a light source
-        draw_queue.AddLight({0,0}, {2, 2, 1.5f}, 200);
+            int scale = 4;
+            int sprite_w = 16;
+            int sprite_h = 24;
+            Rectangle source = Rectangle{0.0f, 0.0f, 0.0f, (float)sprite_h};
+            source.x = (float)(player.lifetime / 10 % 4 * sprite_w);
+            source.y = (ABSF(player.velocity.x) <= 0.001f ? 0.0f : (float)sprite_h);
+            source.width = (float)(player.facing == Facing::RIGHT ? sprite_w : -sprite_w);
+            Rectangle dest = {
+                player.position.x,
+                player.position.y,
+                (float)(scale * sprite_w),
+                (float)(scale * sprite_h)
+            };
+
+            DrawTexturePro(character, source, dest, Vector2{0.0f, 0.0f}, 0.0f, Color{255, 255, 255, 255});
+
+        EndDrawing();
 
         // Render the player
+        /*
         int player_tile_idx;
         if(ABS(player.velocity.x) <= 0.001)
             player_tile_idx = (player.lifetime / 10) % 4;
         else
             player_tile_idx = (player.lifetime / 10) % 4 + 8;
         draw_queue.AddEntity(player.position, texture, player_tile_idx, player.facing == Facing::LEFT);
-
-        // Render a shockwave
-        Shockwave shockwave;
-        float progress = (SDL_GetTicks() % 500) / 500.0f;
-        shockwave.center = glm::vec2(50, 25);
-        shockwave.radius = progress * 100;
-        shockwave.scale = 20;
-        shockwave.strength = 20;
-        draw_queue.AddShockwave(shockwave);
-
-        renderer.render(draw_queue);
-        OS::EndFrame();
+        */
     }
 
-    OS::DestroyContext();
+    UnloadTexture(character);
+
+    CloseWindow();
 
     return 0;
 }
